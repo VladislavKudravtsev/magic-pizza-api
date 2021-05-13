97ff21fdadfc20877d64590fbaab783999254fbc
kipish <avladsir200@gmail.com>
2021-05-13 13:40:24 +0300

    small fixes

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('Connected to db');
+  console.log('Connected to mongodb');
 });
 
 app.listen(process.env.PORT);
